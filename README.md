7a5f43617843791e26e77f2f63c877f96c6c209b
Jiří Nádvorník <nadvornik.jiri@gmail.com>
2018-07-19 20:51:01 +0200

    Working version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# collater
+
+Script for preparing papers for printing (8 pages on 1 sheet of size A3)
+
+usage:
+
+    ./collate.sh Global\ Catastrophic\ Risks\ 2017.pdf test.pdf
+
--- collate.sh
+++ collate.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
+set -e
+set -x
 
-# 2 pages on one
-pdfjam --nup 2x1 --suffix 2up --landscape --outfile out2.pdf 1706.03741.pdf
+outputdir="output"
+tmpdir="tmp-collater"
 
-# this is not working:
-/usr/bin/pdfjam --booklet 'true' --landscape --suffix book --signature '4' --landscape --preamble '\usepackage{everyshi}
-          \makeatletter
-          \EveryShipout{\ifodd\c@page\pdfpageattr{/Rotate 180}\fi}
-          \makeatother
-          ' -- out2.pdf -
+mkdir -p $outputdir
+mkdir -p $tmpdir/
 
+for pdffile in "$@"
+do
+    # 2 pages on one
+    pdfjam --nup 2x1 --suffix 2up --landscape --outfile $tmpdir/out-nup.pdf "$pdffile"
 
+    # # rotate by 180°
+    pdftk $tmpdir/out-nup.pdf cat 1-endwest output $tmpdir/out-rotated.pdf
+
+    # # this is not working:
+    /usr/bin/pdfjam --booklet 'true' --landscape --suffix book --signature '4' --landscape --preamble '\usepackage{everyshi}
+            \makeatletter
+            \EveryShipout{\ifodd\c@page\pdfpageattr{/Rotate 180}\fi}
+            \makeatother
+            ' -o $tmpdir/out-booklet.pdf $tmpdir/out-rotated.pdf
+
+    # rotate every other page by 180 degrees
+    pdftk $tmpdir/out-booklet.pdf cat 1 2-endsouth output $tmpdir/out-reversed.pdf
+
+    # rotate by 90° so it is not a landscape but portrait
+    pdftk $tmpdir/out-reversed.pdf cat 1-endwest output "$outputdir/$pdffile"
+
+done
+
+rm -r $tmpdir
